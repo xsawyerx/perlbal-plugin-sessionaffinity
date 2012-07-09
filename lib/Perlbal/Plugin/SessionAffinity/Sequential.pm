e26e4b254f9bdea86eacfd4a724952547c7e3b87
Sawyer X <xsawyerx@cpan.org>
2012-07-09 15:23:17 +0300

    remove debugging, add check for race condition

--- lib/Perlbal/Plugin/SessionAffinity/Sequential.pm
+++ lib/Perlbal/Plugin/SessionAffinity/Sequential.pm
@@ -3,16 +3,20 @@ use warnings;
 package Perlbal::Plugin::SessionAffinity::Sequential;
 # ABSTRACT: Sequential backend IDs
 
-use DDP;
+my $ref = ref [];
 
 sub get_backend_id {
-    p @_;
     my ( $backend, $create_id ) = @_;
     my @nodes   = @{ $backend->{'service'}{'pool'}{'nodes'} };
 
     # find the id of the node
     # (index number, starting from 1)
     foreach my $i ( 0 .. scalar @nodes ) {
+        # check if it was just removed
+        # stupid race condition...
+        defined $nodes[$i] && ref $nodes[$i] && ref $nodes[$i] eq $ref
+            or next;
+
         my ( $ip, $port ) = @{ $nodes[$i] };
 
         if ( $backend->{'ipport'} eq "$ip:$port" ) {
